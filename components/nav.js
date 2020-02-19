7ddb1510b28fef713c8957833137fd0965acabfc
Dimpos Sitorus <dimpossitorus@gmail.com>
2020-02-19 21:18:54 +0700

    [HOTFIX] Add nav style

--- components/nav.js
+++ components/nav.js
@@ -10,6 +10,28 @@ const Nav = () => (
         </Link>
       </li>
     </ul>
+
+    <style jsx>{`
+      nav {
+        text-align: center;
+      }
+      ul {
+        display: flex;
+        justify-content: space-between;
+      }
+      nav > ul {
+        padding: 4px 16px;
+      }
+      li {
+        display: flex;
+        padding: 6px 8px;
+      }
+      a {
+        color: #067df7;
+        text-decoration: none;
+        font-size: 13px;
+      }
+    `}</style>
   </nav>
 )
 
